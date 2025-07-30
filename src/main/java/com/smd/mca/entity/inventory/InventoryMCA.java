549ac42d495b3d0927b82b821b5afbf1248bcb82
2826961154 <2826961154@qq.com>
2025-07-30 11:13:17 +0800

    Update InventoryMCA.java

--- src/main/java/com/smd/mca/entity/inventory/InventoryMCA.java
+++ src/main/java/com/smd/mca/entity/inventory/InventoryMCA.java
@@ -10,11 +10,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class InventoryMCA extends InventoryBasic {
-    private EntityVillagerMCA villager;
+    private final EntityVillagerMCA villager;
 
     public InventoryMCA(EntityVillagerMCA villager) {
         super("Villager Inventory", true, 27);
@@ -22,93 +20,91 @@ public class InventoryMCA extends InventoryBasic {
     }
 
     public int getFirstSlotContainingItem(Item item) {
-        for (int i = 0; i < this.getSizeInventory(); i++) {
-            ItemStack stack = this.getStackInSlot(i);
-            if (stack.getItem() != item) continue;
-            return i;
+        for (int i = 0; i < getSizeInventory(); i++) {
+            ItemStack stack = getStackInSlot(i);
+            if (!stack.isEmpty() && stack.getItem() == item) {
+                return i;
+            }
         }
         return -1;
     }
 
-    public boolean contains(Class clazz) {
-        for (int i = 0; i < this.getSizeInventory(); ++i) {
-            final ItemStack stack = this.getStackInSlot(i);
-            final Item item = stack.getItem();
-
-            if (item.getClass() == clazz) return true;
+    public boolean contains(Class<?> clazz) {
+        for (int i = 0; i < getSizeInventory(); i++) {
+            Item item = getStackInSlot(i).getItem();
+            if (item.getClass() == clazz) {
+                return true;
+            }
         }
         return false;
     }
 
-    /**
-     * Gets the best quality (max damage) item of the specified type that is in the inventory.
-     *
-     * @param type The class of item that will be returned.
-     * @return The item stack containing the item of the specified type with the highest max damage.
-     */
-    public ItemStack getBestItemOfType(@Nullable Class type) {
-        if (type == null) return ItemStack.EMPTY;
-        else return getStackInSlot(getBestItemOfTypeSlot(type));
+    public ItemStack getBestItemOfType(@Nullable Class<?> type) {
+        int slot = getBestItemOfTypeSlot(type);
+        return slot >= 0 ? getStackInSlot(slot) : ItemStack.EMPTY;
     }
 
-    public ItemStack getBestArmorOfType(EntityEquipmentSlot slot) {
-        ItemStack returnStack = ItemStack.EMPTY;
+    public int getBestItemOfTypeSlot(@Nullable Class<?> type) {
+        if (type == null) return -1;
 
-        List<ItemStack> armors = new ArrayList();
-        for (int i = 0; i < this.getSizeInventory(); ++i) {
-            ItemStack stack = this.getStackInSlot(i);
-            if (stack.getItem() instanceof ItemArmor) {
-                ItemArmor armor = (ItemArmor) stack.getItem();
-                if (armor.armorType == slot) armors.add(stack);
-            }
-        }
-
-        int highestMaxDamage = 0;
-        for (ItemStack stack : armors) {
-            if (stack.getMaxDamage() > highestMaxDamage) {
-                returnStack = stack;
-                highestMaxDamage = stack.getMaxDamage();
-            }
-        }
-        return returnStack;
-    }
-
-    public int getBestItemOfTypeSlot(Class type) {
-        int highestMaxDamage = 0;
         int best = -1;
+        int highestDamage = -1;
 
-        for (int i = 0; i < this.getSizeInventory(); ++i) {
-            ItemStack stackInInventory = this.getStackInSlot(i);
+        for (int i = 0; i < getSizeInventory(); i++) {
+            ItemStack stack = getStackInSlot(i);
+            Item item = stack.getItem();
 
-            final String itemClassName = stackInInventory.getItem().getClass().getName();
-
-            if (itemClassName.equals(type.getName()) && highestMaxDamage < stackInInventory.getMaxDamage()) {
-                highestMaxDamage = stackInInventory.getMaxDamage();
-                best = i;
+            if (item.getClass() == type) {
+                int damage = stack.getMaxDamage();
+                if (damage > highestDamage) {
+                    best = i;
+                    highestDamage = damage;
+                }
             }
         }
 
         return best;
     }
 
+    public ItemStack getBestArmorOfType(EntityEquipmentSlot slot) {
+        ItemStack bestArmor = ItemStack.EMPTY;
+        int maxDamage = -1;
+
+        for (int i = 0; i < getSizeInventory(); i++) {
+            ItemStack stack = getStackInSlot(i);
+            if (!stack.isEmpty() && stack.getItem() instanceof ItemArmor) {
+                ItemArmor armor = (ItemArmor) stack.getItem();
+                if (armor.armorType == slot) {
+                    int damage = stack.getMaxDamage();
+                    if (damage > maxDamage) {
+                        bestArmor = stack;
+                        maxDamage = damage;
+                    }
+                }
+            }
+        }
+        return bestArmor;
+    }
+
     public void dropAllItems() {
-        for (int i = 0; i < this.getSizeInventory(); i++) {
-            ItemStack stack = this.getStackInSlot(i);
-            villager.entityDropItem(stack, 1.0F);
+        for (int i = 0; i < getSizeInventory(); i++) {
+            ItemStack stack = getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                villager.entityDropItem(stack, 1.0F);
+            }
         }
     }
 
     public void readInventoryFromNBT(NBTTagList tagList) {
-        for (int i = 0; i < this.getSizeInventory(); ++i) {
-            this.setInventorySlotContents(i, ItemStack.EMPTY);
+        for (int i = 0; i < getSizeInventory(); i++) {
+            setInventorySlotContents(i, ItemStack.EMPTY);
         }
 
-        for (int i = 0; i < tagList.tagCount(); ++i) {
+        for (int i = 0; i < tagList.tagCount(); i++) {
             NBTTagCompound nbt = tagList.getCompoundTagAt(i);
             int slot = nbt.getByte("Slot") & 255;
-
-            if (slot < this.getSizeInventory()) {
-                this.setInventorySlotContents(slot, new ItemStack(nbt));
+            if (slot < getSizeInventory()) {
+                setInventorySlotContents(slot, new ItemStack(nbt));
             }
         }
     }
@@ -116,17 +112,15 @@ public class InventoryMCA extends InventoryBasic {
     public NBTTagList writeInventoryToNBT() {
         NBTTagList tagList = new NBTTagList();
 
-        for (int i = 0; i < this.getSizeInventory(); ++i) {
-            ItemStack itemstack = this.getStackInSlot(i);
-
-            if (itemstack != ItemStack.EMPTY) {
+        for (int i = 0; i < getSizeInventory(); i++) {
+            ItemStack stack = getStackInSlot(i);
+            if (!stack.isEmpty()) {
                 NBTTagCompound nbt = new NBTTagCompound();
                 nbt.setByte("Slot", (byte) i);
-                itemstack.writeToNBT(nbt);
+                stack.writeToNBT(nbt);
                 tagList.appendTag(nbt);
             }
         }
-
         return tagList;
     }
 }
