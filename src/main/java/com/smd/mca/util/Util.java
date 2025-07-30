9be865df0eff79cc7638732b4e38780f40feebc3
2826961154 <2826961154@qq.com>
2025-07-30 11:21:47 +0800

    优化方法的实现

--- src/main/java/com/smd/mca/util/Util.java
+++ src/main/java/com/smd/mca/util/Util.java
@@ -4,114 +4,126 @@ import com.google.common.base.Optional;
 import com.google.gson.Gson;
 import com.smd.mca.core.MCA;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.apache.commons.io.IOUtils;
 
 import javax.annotation.Nullable;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
-import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 public class Util {
     private static final String RESOURCE_PREFIX = "assets/mca/";
 
     /**
-     * Finds a y position given an x,y,z coordinate triple that is assumed to be the world's "ground".
-     *
-     * @param world	The world in which blocks will be tested
-     * @param x			X coordinate
-     * @param y			Y coordinate, used as the starting height for finding ground.
-     * @param z			Z coordinate
-     * @return Integer representing the air block above the first non-air block given the provided ordered triples.
+     * 获取地面以上安全出生点（第一个空气方块上方）。
      */
-    public static int getSpawnSafeTopLevel(World world, int x, int y, int z)
-    {
-        Block block = Blocks.AIR;
-        while (block == Blocks.AIR && y > 0) {
+    public static int getSpawnSafeTopLevel(World world, int x, int y, int z) {
+        BlockPos pos;
+        IBlockState state;
+
+        while (y > 0) {
+            pos = new BlockPos(x, y, z);
+            state = world.getBlockState(pos);
+            if (!state.getBlock().isAir(state, world, pos)) {
+                break;
+            }
             y--;
-            block = world.getBlockState(new BlockPos(x, y, z)).getBlock();
         }
 
         return y + 1;
     }
 
+    /**
+     * 从 mod JAR 中读取资源为字符串。
+     */
     public static String readResource(String path) {
-        String data;
         String location = RESOURCE_PREFIX + path;
 
-        try {
-            data = IOUtils.toString(new InputStreamReader(MCA.class.getClassLoader().getResourceAsStream(location)));
+        try (InputStreamReader reader = new InputStreamReader(
+                MCA.class.getClassLoader().getResourceAsStream(location))) {
+            return IOUtils.toString(reader);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read resource from JAR: " + location);
+            throw new RuntimeException("Failed to read resource from JAR: " + location, e);
         }
-
-        return data;
     }
 
+    /**
+     * 将资源内容解析为 JSON 对象。
+     */
     public static <T> T readResourceAsJSON(String path, Class<T> type) {
         Gson gson = new Gson();
-        T data = gson.fromJson(Util.readResource(path), type);
-        return data;
+        return gson.fromJson(readResource(path), type);
     }
 
+    /**
+     * 通过 UUID 获取实体。
+     */
     public static Optional<Entity> getEntityByUUID(World world, UUID uuid) {
         for (Entity entity : world.loadedEntityList) {
-            if (entity.getUniqueID().equals(uuid)) {
+            if (uuid.equals(entity.getUniqueID())) {
                 return Optional.of(entity);
             }
         }
         return Optional.absent();
     }
 
+    /**
+     * 获取指定类型的 UUID 对应实体。
+     */
     public static <T extends Entity> Optional<T> getEntityByUUID(World world, UUID uuid, Class<? extends T> clazz) {
         for (Entity entity : world.loadedEntityList) {
-            if (entity.getClass().isAssignableFrom(clazz) && entity.getUniqueID().equals(uuid)) {
+            if (clazz.isAssignableFrom(entity.getClass()) && uuid.equals(entity.getUniqueID())) {
                 return Optional.of((T) entity);
             }
         }
         return Optional.absent();
     }
 
-    public static List<BlockPos> getNearbyBlocks(BlockPos origin, World world, @Nullable Class filter, int xzDist, int yDist) {
-        final List<BlockPos> pointsList = new ArrayList<>();
+    /**
+     * 获取附近指定距离内所有符合条件的方块坐标。
+     */
+    public static List<BlockPos> getNearbyBlocks(BlockPos origin, World world, @Nullable Class<? extends Block> filter, int xzDist, int yDist) {
+        List<BlockPos> result = new ArrayList<>();
+        int ox = origin.getX(), oy = origin.getY(), oz = origin.getZ();
+
         for (int x = -xzDist; x <= xzDist; x++) {
             for (int y = -yDist; y <= yDist; y++) {
                 for (int z = -xzDist; z <= xzDist; z++) {
-                    if (x != 0 || y != 0 || z != 0) {
-                        BlockPos pos = new BlockPos(origin.getX() + x, origin.getY() + y, origin.getZ() + z);
-                        if (filter != null && filter.isAssignableFrom(world.getBlockState(pos).getBlock().getClass())) {
-                            pointsList.add(pos);
-                        } else if (filter == null) {
-                            pointsList.add(pos);
-                        }
+                    if (x == 0 && y == 0 && z == 0) continue;
+
+                    BlockPos pos = new BlockPos(ox + x, oy + y, oz + z);
+                    Block block = world.getBlockState(pos).getBlock();
+
+                    if (filter == null || filter.isAssignableFrom(block.getClass())) {
+                        result.add(pos);
                     }
                 }
             }
         }
-        return pointsList;
+        return result;
     }
 
+    /**
+     * 获取距离 origin 最近的方块位置。
+     */
     public static BlockPos getNearestPoint(BlockPos origin, List<BlockPos> blocks) {
-        double closest = 100.0D;
-        BlockPos returnPoint = null;
-        for (BlockPos point : blocks) {
-            double distance = origin.getDistance(point.getX(), point.getY(), point.getZ());
-            if (distance < closest) {
-                closest = distance;
-                returnPoint = point;
+        BlockPos nearest = null;
+        double minDistSq = Double.MAX_VALUE;
+
+        for (BlockPos target : blocks) {
+            double distSq = origin.distanceSq(target);
+            if (distSq < minDistSq) {
+                minDistSq = distSq;
+                nearest = target;
             }
         }
 
-        return returnPoint;
+        return nearest;
     }
 }
